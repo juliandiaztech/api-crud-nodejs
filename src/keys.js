15e4cad6fcf21dc59a547b61e8ee089a7b7558f0
juliandiazramires <juliancuentaandroid@gmail.com>
2021-03-28 21:30:39 -0500

    update file database of sql and keys.js

--- src/keys.js
+++ src/keys.js
@@ -1,10 +1,10 @@
 module.exports = {
 
     database: {
-        host: 'dev-superfuds.cm8heorrngqo.sa-east-1.rds.amazonaws.com',
-        user: 'testuser',
-        password: '$up3rFud$',
-        database: 'test',
-        port: 3306
+        host: '',
+        user: '',
+        password: '',
+        database: '',
+        port: 
     }
 }
